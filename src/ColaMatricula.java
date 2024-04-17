e67761db21f0489577b5467da334766d06f083b1
Alejandra.D <alejandra.davila.tipan@udla.edu.ec>
2024-04-17 17:43:36 -0500

    limpiarCampos funcion

--- src/ColaMatricula.java
+++ src/ColaMatricula.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Cola {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColaMatricula {
+    private Queue<Vehiculo> carros;
+
+    public ColaMatricula() {
+        carros = new LinkedList<Vehiculo>();
+    }
+
+    public void encolar(String propietario, String marca, int anio){
+        carros.add(new Vehiculo(propietario, marca, anio));
+    }
+    public Vehiculo desencolar() throws Exception{
+        if(carros.isEmpty())
+            throw new Exception("no hay vehiculos en la cola");
+        return carros.poll(); //Pop en pilas, poll en colas
+    }
+    public String listarVehiculos(){
+        String mensaje="";
+        for(Vehiculo v:carros){
+            mensaje += v.toString();
+        }
+        return mensaje;
+    }
+
 }
--- src/Ventana.java
+++ src/Ventana.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Ventana {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ventana {
+    private JPanel Ventana;
+    private JTextField textPropietario;
+    private JComboBox comboMarca;
+    private JTextField textAnio;
+    private JButton agregarALaColaButton;
+    private JButton matricularButton;
+    private JTextArea textArea1;
+    private ColaMatricula matriculas = new ColaMatricula();
+
+    public Ventana(){
+
+        agregarALaColaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                matriculas.encolar(textPropietario.getText(),comboMarca.getSelectedItem().toString(),
+                    Integer.parseInt(textAnio.getText()));
+                textArea1.setText(matriculas.listarVehiculos());
+                limpiarCampos();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ventana");
+        frame.setContentPane(new Ventana().Ventana);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(300,400);
+        frame.setVisible(true);
+    }
+    public void limpiarCampos(){
+        textPropietario.setText("");
+        textAnio.setText("");
+        comboMarca.setSelectedIndex(0);
+    }
 }
